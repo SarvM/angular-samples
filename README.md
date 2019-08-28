937db0daddf35edc6ada9cb750da543ed00a0f10
SarvM <saravana.msi@outlook.com>
2019-08-28 16:29:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5jbh1j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5jbh1j)
---
+++ src/app/play-pipe/play-pipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-pipe',
+  templateUrl: './play-pipe.component.html',
+  styleUrls: ['./play-pipe.component.css']
+})
+export class PlayPipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
