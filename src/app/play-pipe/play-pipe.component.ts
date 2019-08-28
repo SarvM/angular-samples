f7dd7c357d960d927b70e489fdee18a7cbda60c6
SarvM <saravana.msi@outlook.com>
2019-08-28 16:57:35 +0530

    2 files modified, 1 added

--- src/app/play-pipe/play-pipe.component.ts
+++ src/app/play-pipe/play-pipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-play-pipe',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayPipeComponent implements OnInit {
 
+  birthday = new Date(1988, 3, 15);
+  toggle = true;
+  selectFormat = 'short';
+  @ViewChild('s', {static: false}) sRef: ElementRef; 
+
+  get format() {
+    return this.toggle ? 'shortDate' : 'fullDate';
+  }
+
   constructor() { }
 
+  onChange(sel: HTMLInputElement) {
+    this.selectFormat = sel.value;
+  }
+
   ngOnInit() {
+  }
+
+  toggleFormat() {
+    this.toggle = !this.toggle;
   }
 
 }
